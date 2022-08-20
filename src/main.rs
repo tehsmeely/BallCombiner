f5bd25142b79899917f8c52f58e78e04b16f4b0c
Jonty <jon.heiser@gmail.com>
2022-08-20 15:04:25 +0100

    Add balance and readout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
 use bevy::render::texture::ImageSettings;
 use bevy::sprite::{Anchor, Material2d, MaterialMesh2dBundle};
 use bevy_rapier2d::prelude::*;
+use std::collections::HashMap;
 
 fn main() {
     let rapier: RapierPhysicsPlugin<NoUserData> = RapierPhysicsPlugin::pixels_per_meter(32f32);
@@ -9,13 +11,50 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(rapier)
         .add_plugin(RapierDebugRenderPlugin::default())
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .insert_resource(ImageSettings::default_nearest())
         .add_startup_system(setup)
         .add_startup_system(spawn_cups)
         .add_startup_system(spawn_balance)
+        .insert_resource(BalanceCounter::new())
         .add_system(spawn_ball_system)
         .add_system(rotate_cup_system)
+        .add_system(ball_sensor_system)
         .run();
+}
+
+#[derive(Debug, Clone)]
+pub struct BalanceCounter {
+    ball_count: HashMap<usize, usize>,
+}
+
+impl BalanceCounter {
+    fn new() -> Self {
+        BalanceCounter {
+            ball_count: HashMap::new(),
+        }
+    }
+
+    fn incr(&mut self, ball_id: usize) {
+        *self.ball_count.entry(ball_id).or_insert(0) += 1;
+    }
+
+    fn decr(&mut self, ball_id: usize) {
+        match self.ball_count.get_mut(&ball_id) {
+            Some(0) => warn!("Tried to decr 0 in map (key: {}), not doing.", ball_id),
+            Some(val) => *val -= 1,
+            None => warn!("Tried to decr nonexistent count (key: {})", ball_id),
+        }
+    }
+
+    fn total_count(&self) -> usize {
+        let mut i = 0;
+        for v in self.ball_count.values() {
+            i += v;
+        }
+        i
+    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -63,8 +102,68 @@ fn rotate_cup_system(mut cup_query: Query<(&mut Velocity, &Cup)>, input: Res<Inp
     }
 }
 
+fn other_entity_if_match(match_entity: &Entity, e1: Entity, e2: Entity) -> Option<Entity> {
+    if e1 == *match_entity {
+        Some(e2)
+    } else if e2 == *match_entity {
+        Some(e1)
+    } else {
+        None
+    }
+}
+
+fn ball_sensor_system(
+    mut active_events: EventReader<CollisionEvent>,
+    balance_sensor_query: Query<Entity, With<BalanceSensor>>,
+    ball_query: Query<&Ball>,
+    mut balance_text_query: Query<&mut Text, With<BalanceText>>,
+    mut balance_counter: ResMut<BalanceCounter>,
+) {
+    if !active_events.is_empty() {
+        let balance_sensor_entity = balance_sensor_query.single();
+        let mut counter_changed = false;
+        for event in active_events.iter() {
+            match event {
+                CollisionEvent::Started(e1, e2, _flags) => {
+                    if let Some(other_entity) =
+                        other_entity_if_match(&balance_sensor_entity, *e1, *e2)
+                    {
+                        if let Ok(ball) = ball_query.get(other_entity) {
+                            balance_counter.incr(ball.0);
+                            counter_changed = true;
+                        }
+                    }
+                }
+                CollisionEvent::Stopped(e1, e2, _flags) => {
+                    if let Some(other_entity) =
+                        other_entity_if_match(&balance_sensor_entity, *e1, *e2)
+                    {
+                        if let Ok(ball) = ball_query.get(other_entity) {
+                            balance_counter.decr(ball.0);
+                            counter_changed = true;
+                        }
+                    }
+                }
+            }
+        }
+        if counter_changed {
+            println!("Counter changed: {:?}", balance_counter);
+            let total_mass = 0.8 * (balance_counter.total_count() as f32);
+            if let Ok(mut text) = balance_text_query.get_single_mut() {
+                text.sections[0].value = format!("{:.2}", total_mass);
+            }
+        }
+    }
+}
+
 #[derive(Component)]
 struct Cup(usize);
+#[derive(Component)]
+struct Ball(usize);
+#[derive(Component)]
+struct BalanceSensor;
+#[derive(Component)]
+struct BalanceText;
 
 fn spawn_cups(mut commands: Commands, asset_server: Res<AssetServer>) {
     spawn_cup(-50.0, 0, &mut commands, &asset_server);
@@ -129,6 +228,7 @@ fn spawn_ball(x_offset: f32, idx: usize, commands: &mut Commands, asset_server: 
         .insert(Sleeping::disabled())
         .insert(Ccd::enabled())
         .insert(GravityScale(1.0))
+        .insert(Ball(idx))
         .insert_bundle(SpriteBundle {
             //transform: Transform::from_xyz(0.0, 0.0, 0.0),
             sprite: Sprite {
@@ -144,6 +244,7 @@ fn spawn_ball(x_offset: f32, idx: usize, commands: &mut Commands, asset_server: 
 }
 
 fn spawn_balance(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("Quicksand-Regular.ttf");
     let (collider_shape, sensor_shape) = {
         let y_offset = -6.0;
         let thickness = 4.0;
@@ -170,6 +271,7 @@ fn spawn_balance(mut commands: Commands, asset_server: Res<AssetServer>) {
     };
 
     let transform = Transform::from_xyz(0.0, -70.0, 0.0);
+    let text_transform = Transform::from_xyz(-8.0, -96.0, 1.0);
     let texture = asset_server.load("balance.png");
     commands
         .spawn()
@@ -185,6 +287,29 @@ fn spawn_balance(mut commands: Commands, asset_server: Res<AssetServer>) {
                 .spawn()
                 .insert(sensor_shape)
                 .insert(Sensor)
+                .insert(ActiveEvents::COLLISION_EVENTS)
+                .insert(BalanceSensor)
                 .insert(Transform::from_xyz(0.0, 10.0, 0.0));
         });
+    commands
+        .spawn()
+        .insert_bundle(Text2dBundle {
+            text: Text::from_section(
+                "0.00",
+                TextStyle {
+                    font,
+                    font_size: 10.0,
+                    color: Default::default(),
+                },
+            )
+            .with_alignment(TextAlignment {
+                vertical: VerticalAlign::Bottom,
+                horizontal: HorizontalAlign::Center,
+            }),
+            transform: text_transform,
+            text_2d_size: Default::default(),
+            text_2d_bounds: Default::default(),
+            ..default()
+        })
+        .insert(BalanceText);
 }
